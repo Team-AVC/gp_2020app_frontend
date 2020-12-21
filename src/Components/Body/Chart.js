bd98946710b89c510d72cc2c21a55850d3590390
CharithNiroshan <charithniroshan44@gmail.com>
2020-12-22 01:10:46 +0530

    commit created the lastvalue component and chart component

--- src/Components/Body/Chart.js
+++ src/Components/Body/Chart.js
@@ -1,8 +1,29 @@
-import React from "react";
-import "./Chart.css";
+import React, { useState } from "react";
+import { Line } from "react-chartjs-2";
 
 function Chart() {
-  return <div className="chart"></div>;
+  const [chartData, setChartData] = useState({
+    labels: [
+      "Boston",
+      "Worcester",
+      "Springfield",
+      "Lowell",
+      "Cambridge",
+      "New Bedford",
+    ],
+    datasets: [
+      {
+        label: "Population",
+        data: [617594, 181045, 153060, 106519, 105162, 95072],
+      },
+    ],
+  });
+
+  return (
+    <div>
+      <Line data={chartData} height="350" width="600" />
+    </div>
+  );
 }
 
 export default Chart;
--- src/Components/Body/Lastvalue.js
+++ src/Components/Body/Lastvalue.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Lastvalue.css";
 
-function Lastvalue() {
-  return <div className="lastvalue"></div>;
+function Lastvalue(props) {
+  return (
+    <div className="lastvalue">
+      <img
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fscience-thermometer_71296&psig=AOvVaw19_EMubOc3Cs_SeSWHmrp8&ust=1608384799912000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCNq7fS1-0CFQAAAAAdAAAAABAD"
+        alt="thermometer"
+      />
+      <div className="lastvalue_details">
+        <h2>Outdoor Temperature</h2>
+        <h3>16 Seconds Ago</h3>
+      </div>
+      <h2 className="value">49.7 F</h2>
+    </div>
+  );
 }
 
 export default Lastvalue;
