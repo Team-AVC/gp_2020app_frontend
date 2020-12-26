06bbc2630f7f57c9a9c4eeb680d210cb972c3493
BimaliWickramasinghe <bimalicw@gmail.com>
2020-12-26 13:06:44 +0530

    Add reducer.js

--- src/ContextAPI/reducer.js
+++ src/ContextAPI/reducer.js
@@ -1,1 +1,34 @@
-
+export const initialState = {
+    activecensor: null,
+    censordata: {
+      lastreading: {},
+      alerts: [],
+      lastreadings: [],
+    },
+    censors: [],
+  };
+  
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_ACTIVECENSOR":
+        return {
+          ...state,
+          activecensor: action.value,
+        };
+      case "SEt_CENSORS":
+        return {
+          ...state,
+          censors: action.value,
+        };
+      case "SET_CENSORDATA":
+        return {
+          ...state,
+          censordata: action.value,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
+  
