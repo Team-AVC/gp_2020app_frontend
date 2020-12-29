4977b513d5d31e87e4365f6e152cf4f1c74252bb
BimaliWickramasinghe <bimalicw@gmail.com>
2020-12-29 22:09:46 +0530

    Updated reducer.js

--- src/ContextAPI/reducer.js
+++ src/ContextAPI/reducer.js
@@ -1,33 +1,30 @@
 export const initialState = {
-    activecensor: null,
-    censordata: {
-      lastreading: {},
-      alerts: [],
-      lastreadings: [],
-    },
-    censors: [],
-  };
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "SET_ACTIVECENSOR":
-        return {
-          ...state,
-          activecensor: action.value,
-        };
-      case "SEt_CENSORS":
-        return {
-          ...state,
-          censors: action.value,
-        };
-      case "SET_CENSORDATA":
-        return {
-          ...state,
-          censordata: action.value,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default reducer;
+  activeCensorid: undefined,
+  activeCensor: {},
+  censors: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_ACTIVECENSOR":
+      return {
+        ...state,
+        activeCensor: action.value,
+      };
+    case "SET_CENSORS":
+      return {
+        ...state,
+        censors: action.value,
+      };
+    case "SET_ACTIVECENSORID":
+      return {
+        ...state,
+        activeCensorid: action.value,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
   
