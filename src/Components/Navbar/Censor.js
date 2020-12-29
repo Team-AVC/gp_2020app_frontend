44f29b002dc5faf14ce99c342670ce2692fe3bbb
BimaliWickramasinghe <bimalicw@gmail.com>
2020-12-29 23:59:31 +0530

    Add js and css to censor

--- src/Components/Navbar/Censor.js
+++ src/Components/Navbar/Censor.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { useDataLayerValue } from "../../ContextAPI/datalayer";
 import "./Censor.css";
 
-function Censor() {
-  return <div className="censor"></div>;
+function Censor({ title, id }) {
+  const [{ censors, activeCensorid }, dispatch] = useDataLayerValue();
+
+  const handleClick = () => {
+    dispatch({
+      type: "SET_ACTIVECENSORID",
+      value: id,
+    });
+
+    censors?.map((censor) => {
+      if (id === censor.sensorId) {
+        console.log(censor);
+        dispatch({
+          type: "SET_ACTIVECENSOR",
+          value: censor,
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="censor" onClick={() => handleClick()}>
+      <h2>{title}</h2>
+    </div>
+  );
 }
 
 export default Censor;
+
