f2676d17a15bffe203b7a0040dcda38bd352eba0
BimaliWickramasinghe <bimalicw@gmail.com>
2020-12-30 17:47:39 +0530

    Add js and css files to body

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Body.css";
+import Lastvalue from "./Lastvalue";
+import Chart from "./Chart";
+import Table from "./Table";
 
 function Body() {
+  const [isAlerts, setAlerts] = useState(false);
+  const [isReadings, setReadings] = useState(true);
+
+  const handleClick = (e) => {
+    if (e.target.id === "readings") {
+      setAlerts(false);
+      setReadings(true);
+    } else {
+      setAlerts(true);
+      setReadings(false);
+    }
+  };
+
   return (
     <div className="body">
-      <h1>This is the Body</h1>
+      <Lastvalue />
+      <div className="selection">
+        <div className="option">
+          Past Censor Readings
+          <input
+            type="radio"
+            checked={isReadings}
+            id="readings"
+            onChange={(e) => {
+              handleClick(e);
+            }}
+          ></input>
+        </div>
+        <div className="option">
+          Past Alerts
+          <input
+            type="radio"
+            id="alerts"
+            checked={isAlerts}
+            onChange={(e) => {
+              handleClick(e);
+            }}
+          ></input>
+        </div>
+      </div>
+      {isReadings ? <Chart /> : <Table />}
     </div>
   );
 }
