6cfb26419398ff095624e6bb869b148da5714586
BimaliWickramasinghe <bimalicw@gmail.com>
2020-12-30 17:49:10 +0530

    Add js and css file to table component

--- src/Components/Body/Table.js
+++ src/Components/Body/Table.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDataLayerValue } from "../../ContextAPI/datalayer";
 import "./Table.css";
 
 function Table() {
-  return <div className="table"></div>;
+  const [{ activeCensor }, dispatch] = useDataLayerValue();
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Alert ID</th>
+          <th>Alert Message</th>
+          <th>Date and Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {activeCensor?.alertDatas.map((alert) => {
+          return (
+            <tr key={alert.alertId}>
+              <td>{alert.alertId}</td>
+              <td>{alert.aMessage}</td>
+              <td>{alert.timestamp}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
